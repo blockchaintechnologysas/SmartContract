eff4051aa92b9dfbc7fbef237d0bd27ec4ee29c1
Blockchain Technology SAS <83485324+blockchaintechnologysas@users.noreply.github.com>
2025-08-11 17:22:09 -0500

    Create token_lactocoin.sol

---
+++ eventos/sena-11agos2025/ejemplo1/token_lactocoin.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+import "@openzeppelin/contracts@4.9.5/access/Ownable.sol";
+import "@openzeppelin/contracts@4.9.5/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.5/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract Tokenstandar is ERC20, ERC20Burnable, Ownable {
+    uint8 private _decimals;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        uint8 decimals_,
+        address owner
+    ) ERC20(name, symbol) {
+        _decimals = decimals_;
+        
+        if (initialSupply > 0) {
+            _mint(owner, initialSupply);
+        }
+        
+        transferOwnership(owner);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public override {
+        super.burn(amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public override {
+        super.burnFrom(account, amount);
+    }
+}
